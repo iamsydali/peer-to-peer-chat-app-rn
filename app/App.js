d4db9180d5f90c9db33c3657320e4dc22f3e3f48
iamsydali <iamsydali@gmail.com>
2025-07-18 01:41:11 +0500

    name fix

--- app/App.js
+++ app/App.js
@@ -2,7 +2,7 @@ import BareProvider from './src/component/BareProvider'
 import ReduxProvider from './src/component/ReduxProvider'
 import HomeScreen from './src/screen/HomeScreen'
 
-import { rpcHandler } from './src/lib/rpcRedux'
+import { rpcHandler } from './src/lib/rpc'
 
 export default function App() {
   return (
